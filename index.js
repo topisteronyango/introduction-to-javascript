672a52c3b08a0efa1298c4834a499b4d23aa9f6a
t-onyango <t.onyango@alustudent.com>
2022-06-22 14:56:53 +0200

    introduction to javascript

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-// document.write("Name: Topister Nandera Onyango");
-
-// document.write("Height: 150cm");
-
-// document.write("Country: Kenya");
 
 alert(' Name: Topister Nandera Onyango\n Height: 150cm\n Country: Kenya')
-// alert('Height: 157cm')
-// alert('Country: Kenya')
